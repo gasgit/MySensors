7623aa164ee2e828c1e878b04ac39d247dcac292
gasgit <glengardiner@gmail.com>
2018-02-06 20:34:44 +0000

    listview replace htmlin textview and webview

--- app/src/main/java/ie/gasgit/mysensors/MainActivity.java
+++ app/src/main/java/ie/gasgit/mysensors/MainActivity.java
@@ -2,47 +2,39 @@ package ie.gasgit.mysensors;
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
+import static ie.gasgit.mysensors.R.layout;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv =null;
+    private ListView sensor_list_view;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
 
-        tv = findViewById(R.id.tv);
+        sensor_list_view = findViewById(R.id.sensor_list_view);
+
 
         SensorManager mySM = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> myListSensors = null;
-        if (mySM != null) {
+
+
+        if(mySM != null) {
             myListSensors = mySM.getSensorList(Sensor.TYPE_ALL);
         }
 
-        if (myListSensors != null) {
-            for (int i = 0; i<myListSensors.size(); i++){
-
-                tv.append(Html.fromHtml(
-                        "<!DOCTYPE html>" +
-                        "<table style='width:100%'>" +
-
-                        "<tr style='background-color:rgb(192,192,192)'><h1 ><font color='#c5c5c5'>" + myListSensors.get(i).getName().toUpperCase() + "</font></h1></tr>" +
-                               "<tr><h2>" + myListSensors.get(i).getVendor() + "</h2></tr>" +
-                                "<tr><h2>" + myListSensors.get(i).getVersion() + "<h3></tr>" +
-                                "</table>"
-
-                ));
 
 
-            }
-        }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myListSensors);
+        sensor_list_view.setAdapter(adapter);
     }
 }
